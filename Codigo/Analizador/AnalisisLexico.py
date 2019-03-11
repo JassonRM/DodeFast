98e9948d50fe710bbfda8dcd19b8a1f34907c573
dario1227 <darior1227@gmail.com>
2019-03-11 17:01:08 -0600

    Funciones coma y dos puntos

--- Codigo/Analizador/AnalisisLexico.py
+++ Codigo/Analizador/AnalisisLexico.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 reservadas=['DCL', 'EnCaso', 'Cuando','EnTons', 'SiNo', 'FINENCASO', 'Repita', 'HastaEncontrar', 'Desde', 'Hasta',
-              'Haga', 'FINDESDE', 'Llamar','Inicio', 'Final', 'Proc','DEFAULT']
+              'Haga', 'FINDESDE', 'Llamar','Inicio', 'Final', 'Proc','DEFAULT','COMA','DPUNTO','FUNCION']
 movimientos=['AF','A','F','DFA','IFA','DFB','IFB','DAA', 'IAA','DAB','IAB','AA']
 tokens = reservadas+['ID','EQUAL', 'GT', 'LT', 'NE', 'GTE', 'LTE', 'PLUS', 'MINUS', 'TIMES',
           'DIVIDE', 'LPARENT', 'RPARENT','NUMBER','RKEY','LKEY','SEMMICOLOM' ,'ASSIGN']
@@ -9,6 +9,8 @@ t_PLUS = r'\+'
 t_ASSIGN = r'='
 t_LKEY=r'\{'
 t_RKEY=r'\}'
+t_COMA=r','
+t_DPUNTO=r':'
 t_MINUS = r'\-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
@@ -21,6 +23,31 @@ t_LTE = r'<='
 t_LPARENT = r'\('
 t_RPARENT = r'\)'
 t_SEMMICOLOM = r';'
+def t_Dec(t):
+    r'Dec'
+    t.value = "FUNCION"
+    t.type = "FUNCION"
+    return t
+def t_Inc(t):
+    r'Inc'
+    t.value = "FUNCION"
+    t.type = "FUNCION"
+    return t
+def t_Mover(t):
+    r'Mover'
+    t.value = "FUNCION"
+    t.type = "FUNCION"
+    return t
+def t_Aleatorio(t):
+    r'Aleatorio'
+    t.value = "FUNCION"
+    t.type = "FUNCION"
+    return t
+def t_Ini(t):
+    r'Ini'
+    t.value = "FUNCION"
+    t.type = "FUNCION"
+    return t
 def t_FinEnCaso(t):
     r'Fin-EnCaso'
     t.value = "FINENCASO"
@@ -39,8 +66,6 @@ def t_ID(t):
 def t_error(t):
     print('Caracter invalido ' + t.value[0])
     t.lexer.skip(1)
-
-
 def t_newLine(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -49,7 +74,7 @@ def t_NUMBER(t):
     t.value=int(t.value)
     return t
 analizador = lex.lex()
-analizador.input("Inicio \n EnCaso \n Cuando \n juan < 12 EnTons \n { Maria } \n  SiNo \n {  MAMELA } \n Fin-EnCaso ; \n Final")
+analizador.input("Aleatorio Inc Ini Dec")
 
 while True:
     tok = analizador.token()
